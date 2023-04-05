933cb7696d974cb32a3b983e3512d8adaec31652
Jason-Vitale <vitale.jason.jr@gmail.com>
2023-04-05 17:02:43 -0400

    Current

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const resturants = [
+const restaurants = [
     {
         name: "Melfi's",
         rating: 9.1,
@@ -19,3 +19,50 @@ const resturants = [
     }
 ]
 
+restaurants.sort((a, b) => b.rating - a.rating);
+//console.log(restaurants);
+
+
+// Get the HTML element where the restaurant list will be displayed
+const restaurantList = document.getElementById("restaurant-list");
+
+// Loop through each restaurant in the array and create an HTML element to represent it
+restaurants.forEach(restaurant => {
+  // Create a new list item element for the restaurant
+  const restaurantItem = document.createElement("li");
+
+  // Add the "restaurant" class to the list item element
+  restaurantItem.classList.add("restaurant");
+
+  // Create a new div element to display the name of the restaurant
+  const name = document.createElement("div");
+  name.classList.add("name");
+  name.textContent = restaurant.name;
+  restaurantItem.appendChild(name);
+
+  // Create a new div element to display the location of the restaurant
+  const location = document.createElement("div");
+  location.classList.add("location");
+  location.textContent = restaurant.location;
+  restaurantItem.appendChild(location);
+
+  // Create a new div element to display the rating of the restaurant
+  const rating = document.createElement("div");
+  rating.classList.add("rating");
+  rating.textContent = `Rating: ${restaurant.rating}`;
+  restaurantItem.appendChild(rating);
+
+  // Create a new div element to display the best dish of the restaurant
+  const bestDish = document.createElement("div");
+  bestDish.classList.add("best-dish");
+  bestDish.textContent = `Best Dish: ${restaurant.bestDish}`;
+  restaurantItem.appendChild(bestDish);
+
+  // Add a click event listener to the restaurant item that toggles the "show" class on the bestDish element
+  restaurantItem.addEventListener("click", () => {
+    bestDish.classList.toggle("show");
+  });
+
+  // Add the restaurant item to the HTML element that displays the restaurant list
+  restaurantList.appendChild(restaurantItem);
+});
