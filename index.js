d94a282e6af41ced1b6a3ba59ad2e63a18d36b6e
Jason-Vitale <vitale.jason.jr@gmail.com>
2023-04-05 16:41:28 -0400

    Added temp res list

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Test");
+const resturants = [
+    {
+        name: "Melfi's",
+        rating: 9.1,
+        location: "Charleston, SC",
+        bestDish: "Calamari"
+    },
+    {
+        name: "Little Jack's",
+        rating: 6.1,
+        location: "Charleston, SC",
+        bestDish: "Tavern Burger"
+    },
+    {
+        name: "McDonalds",
+        rating: 9.9,
+        location: "Everywhere",
+        bestDish: "Everything"
+    }
+]
+
